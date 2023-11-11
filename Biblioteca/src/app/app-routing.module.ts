44390394356dfb5a967d4a4fd17447d60d192493
Fabianaaguero <fabiana.aguero22@gmail.com>
2023-11-11 19:14:23 -0300

    Creación de componentes, ruteo y modelo del producto.

--- Biblioteca/src/app/app-routing.module.ts
+++ Biblioteca/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { TablaComponent } from './Components/tabla/tabla.component';
+import { NosotrosComponent } from './Components/nosotros/nosotros.component';
+import { RegistroComponent } from './Components/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'tabla', component: TablaComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
